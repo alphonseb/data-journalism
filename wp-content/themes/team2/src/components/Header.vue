8bb17f473090591b79b8520ea2b82a4a95042f27
alphonseb <alphonse.bouy@gmail.com>
2019-06-26 11:46:28 +0200

    header

--- wp-content/themes/team2/src/components/Header.vue
+++ wp-content/themes/team2/src/components/Header.vue
@@ -38,6 +38,12 @@ export default {
 header{
     display: flex;
     justify-content: space-between;
+    align-items: center;
+    .logo{
+        img{
+            width: 80px;
+        }
+    }
     .socials{
         a{
             display: inline-flex;
